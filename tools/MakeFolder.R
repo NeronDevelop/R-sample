92412d2b14fea456b8ddf7a9dc7579b6f93160e5
setoguchi mugen <setoguchimugen0@gmail.com>
2023-07-31 20:03:52 +0900

    add: ツールを二つ追加した.

---
+++ tools/MakeFolder.R
@@ -0,0 +1,31 @@
+#
+# TODO: 入れ子に対応したい。
+# 任意のディレクトリを作らいたい.
+#
+MakeFolder <- function(folder_name="") {
+  
+  # エラー出力定義
+  ARG_ERR <- "\nArgments is Invalid!\n"
+  
+  # 現在のディレクトリを取得
+  cur_wd <- getwd()
+  
+  #
+  # 引数のチェック
+  #
+  if (class(folder_name) != "character" | length(folder_name) != 1) {
+    stop(message=ARG_ERR)
+  }
+  
+  if (folder_name == "") {
+    folder_name <- Sys.Date()
+  }
+  
+  # フォルダがなければ作成する
+  folder_path <- paste0(cur_wd, "/", folder_name)
+  if (!file.exists(folder_path)) {
+    dir.create(folder_path)
+  }
+  
+  return(folder_name)
+}
---
+++ tools/csv2yaml.R
@@ -0,0 +1,34 @@
+#
+# データフレームをyamlファイルに変換する
+# TODO: 考えること
+# ファイルを引数にとれないからとれるようにしたほうがいいかも...
+#
+df2yaml <- function(df, output_path="output.yaml", id=TRUE) {
+  # サブ関数
+  output_element <- function(df, row_num, row_name) {
+    return(paste0("    ", row_name, ": ", df[row_num, row_name], "\n"))
+  }
+
+
+  if (nrow(df) <= 0) {
+    return
+  }
+
+  # 列名
+  row_names <- names(df)
+  sink(output_path)
+  cat("---\n")
+  for (row_num in 1:nrow(df)) {
+    cat("-\n")
+    if (id) {
+      cat(paste0("    id: ", row_num, "\n"))
+      for (row_name in row_names) {
+        if (df[row_num, row_name] != "") {
+          cat(output_element (df, row_num, row_name))
+        }
+      }
+    }
+  }
+  sink()
+  return
+}
