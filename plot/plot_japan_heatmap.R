c863bed87292f3f6c5bca9eaec3675f79d5a6d07
NeronDevelop <neron.develop@gmail.com>
2023-08-05 18:21:50 +0900

    edit: 少しソースコードを修正した.

--- plot/plot_japan_heatmap.R
+++ plot/plot_japan_heatmap.R
@@ -8,6 +8,7 @@ PlotMapHeat <- function(df, fill_vec, legend_name, title_str=NULL, x_str=NULL, y
     coord_sf(datum = NA) +
     guides(fill = NULL) +
     theme(panel.background = element_rect(fill = "black", colour = "black")) +
+    theme(text=element_text(size = 20), legend.title=element_text(size = 15)) +
     labs(title=title_str, ) +
     scale_fill_distiller(name = legend_name,　palette = "Oranges", direction = 1)
   return(g)
